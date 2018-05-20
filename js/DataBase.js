c275e7baa890c5afef48db7c366fe588adc8ff52
Dima Usachov <usachovdima1987@gmail.com>
2018-05-20 19:50:42 +0300

    add static BD

---
+++ js/DataBase.js
@@ -0,0 +1,47 @@
+var DataBase = [
+  {
+    id:1,
+    phone:'066123456',
+    name:'Dima',
+    surname:' Usach',
+    company:'CubeX',
+    email:'elefent@i.ua',
+    photo:'http://www.eurosatt.com/GlobalStar/img/gps.gif'
+  },
+  {
+    id:2,
+    phone:'066123450',
+    name:'Kolya',
+    surname:' Bond',
+    company:'CubeX',
+    email:'getadobe@i.ua',
+    photo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQMKoOSz015K7aTMmkINWCyJMjLFiI6V3MXmCWYenRHrgwXsPI'
+  },
+  {
+    id:3,
+    phone:'066123451',
+    name:'Igor',
+    surname:'Super',
+    company:' CubeX',
+    email:'agentX@gmail.com',
+    photo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJkGK6JdcYCdFW7QDeK5aEeLJjQN8zGJfxU9AeQRC26bROVlKc'
+  },
+  {
+    id:4,
+    phone:'066123452',
+    name:'Den',
+    surname:' Ivanov',
+    company:'CubeX',
+    email:'agentY@gmail.com',
+    photo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE_KV0RAJKEA2W_L7CAlxXnXwnHiv59haWzXhMc323iR3c1Szs'
+  },
+  {
+    id:5,
+    phone:'066123453',
+    name:'Kat',
+    surname:' Petrova',
+    company:'CubeX',
+    email:'react@i.ua',
+    photo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIKkGN9O4806PQOdB8gVqJ5KFjB7oJFwXJXzCn8Pni0t6oj_kk'
+  },
+];
